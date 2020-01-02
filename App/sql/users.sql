236b7500f65b44ce780339bb6e0909564d54780d
jcubillos555 <jaime.cubillos@globant.com>
2020-01-02 15:48:41 -0500

    prueba8

--- App/sql/users.sql
+++ App/sql/users.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE [IF NOT EXISTS] crud_app;
+CREATE DATABASE IF NOT EXISTS crud_app;
 USE crud_app;
-CREATE USER [IF NOT EXISTS] 'jcubillos555'@'%' IDENTIFIED BY 'jcubillos555';
+CREATE USER IF NOT EXISTS 'jcubillos555'@'%' IDENTIFIED BY 'jcubillos555';
 FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON * . * TO 'jcubillos555'@'%';
 FLUSH PRIVILEGES;
@@ -11,7 +11,7 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE TABLE [IF NOT EXISTS] `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) UNSIGNED NOT NULL,
   `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
