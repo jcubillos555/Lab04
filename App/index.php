ca533e2193a136d887608c13febb1aa7d8992ce9
jcubillos555 <jaime.cubillos@globant.com>
2020-01-02 11:28:44 -0500

    prueba2

--- App/index.php
+++ App/index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<center>Lab04 - Terraform Commit: "; echo gethostname(); echo "</center>";
+echo "<center>Lab04 - Terraform Commit1: "; echo gethostname(); echo "</center>";
 require 'db_connection.php';
 // function for getting data from database
 function get_all_data($conn){
