12c8aed69f79a6e89a81aa82f1ac78d7adf97abc
jcubillos555 <jaime.cubillos@globant.com>
2020-01-02 16:25:13 -0500

    prueba12

--- App/sql/users.sql
+++ App/sql/users.sql
@@ -12,15 +12,15 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) UNSIGNED NOT NULL, ADD PRIMARY KEY (`id`), MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL,
   `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL, ADD UNIQUE KEY `user_email` (`user_email`)
+  `user_email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---ALTER TABLE `users`
---  ADD PRIMARY KEY (`id`),
---  ADD UNIQUE KEY `user_email` (`user_email`);
+ALTER TABLE `users`
+  ADD IF NOT EXISTS PRIMARY KEY (`id`),
+  ADD IF NOT EXISTS UNIQUE KEY `user_email` (`user_email`);
 
---ALTER TABLE `users`
---  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
